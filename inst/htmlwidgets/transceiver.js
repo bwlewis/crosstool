0738f17c3908e7a60fbc0ba4ba31e35060926fe2
bwlewis <blewis@illposed.net>
2017-07-10 00:28:54 -0400

    transmitter init

--- inst/htmlwidgets/transceiver.js
+++ inst/htmlwidgets/transceiver.js
@@ -16,22 +16,18 @@ HTMLWidgets.widget({
           var val;
           if(Array.isArray(e.value)) {
             // the usual crosstalk selection array values
-            if(x.lookup)
-            {
+            if(x.lookup) {
               val = e.value.map(function(i) {return x.lookup[x.crosstalk_key.indexOf(i)];});
             } else val = e.value;
             if(! Array.isArray(val)) val = [val];
           } else {
             // non-standard selection object value (FIXME we should switch to using _extraInfo)
-            if(x.type && x.type == "object")
-            {
-              if(x.lookup)
-              {
+            if(x.type && x.type == "object") {
+              if(x.lookup) {
                 val = {object: x.lookup[x.crosstalk_key.indexOf(e.value.object)]};
               } else val = {object: e.value.object};
             } else {
-              if(x.lookup)
-              {
+              if(x.lookup) {
                 val = x.lookup[x.crosstalk_key.indexOf(e.value.object)];
               } else val = e.value.object;
               if(! Array.isArray(val)) val = [val];
@@ -40,16 +36,21 @@ HTMLWidgets.widget({
           ct_sel2.set(val);
         };
         ct_sel1.on("change", cf);
-        if(x.init)
-        {
-          if(x.type && x.type == "object")
-          {
-            if(x.lookup) cf({sender: null, value: {object: x.lookup[0]}});
-            else cf({sender: null, value: {object: 0}});
-          } else {
-            if(x.lookup) cf({sender: null, value: [x.lookup[0]]});
-            else cf({sender: null, value: [0]});
+        if(x.init) {
+          if(x.type && x.type == "object") {
+            if(x.lookup) cf({sender: null, value: {object: x.lookup[x.init]}});
+            else cf({sender: null, value: {object: x.init}});
+            return;
           }
+          var val;
+          if(Array.isArray(x.init)) {
+            if(x.lookup) val = x.init.map(function(i) {return x.lookup[x.crosstalk_key.indexOf(i)];});
+            else val = x.init;
+          } else {
+            if(x.lookup) val = [x.lookup[x.crosstalk_key.indexOf(x.init)]];
+            else val = [x.init];
+          }
+          cf({sender: null, value: val});
         }
       },
       resize: function(width, height) { }
--- inst/htmlwidgets/transmitter.js
+++ inst/htmlwidgets/transmitter.js
@@ -12,13 +12,34 @@ HTMLWidgets.widget({
           ct_filter.setGroup(x.crosstalk_group);
         }
         el.innerHTML = x.innerHTML;
-        el.children[0].addEventListener("change", function() {
-          if(x.type && x.type == "object")
-          {
-            // non-standard selection object value (FIXME we should switch to using _extraInfo)
-            ct_sel.set({object: el.children[0][x.value]});
-          } else ct_sel.set([el.children[0][x.value]]);
-        });
+        if(el.children && el.children.length > 0) {
+          el.children[0].addEventListener("change", function() {
+            if(x.type && x.type == "object") {
+              // non-standard selection object value (FIXME switch to use _extraInfo)
+              ct_sel.set({object: el.children[0][x.value]});
+            } else {
+              var val = el.children[0][x.value];
+              if(!Array.isArray(val)) val = [val];
+              ct_sel.set(val);
+            }
+          });
+        }
+        if(x.init) {
+          if(x.type && x.type == "object") {
+            if(x.lookup) ct_sel.set({object: x.lookup[x.init]});
+            else ct_sel.set({object: x.init});
+            return;
+          }
+          var val;
+          if(Array.isArray(x.init)) {
+            if(x.lookup) val = x.init.map(function(i) {return x.lookup[x.crosstalk_key.indexOf(i)];});
+            else val = x.init;
+          } else {
+            if(x.lookup) val = [x.lookup[x.crosstalk_key.indexOf(x.init)]];
+            else val = [x.init];
+          }
+          ct_sel.set(val);
+        }
       },
       resize: function(width, height) { }
     };
